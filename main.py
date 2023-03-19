b1d3f515830258762b4c25ea101fd119a3400d46
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-03-19 11:40:06 -0300

    update main.py

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ b2 = BucketS3('datalake-my-lucas-bucket')
 b2.download('raw-data/ITENS_PROVA_2020.csv',
             'DADOS/ITENS_PROVA_2020.csv')
 
-# Converting MICRODADOS_ENEM_2020 from csv to parquet
+# Converting from csv to parquet
 df1 = Convert()
 df1.read_csv('DADOS\MICRODADOS_ENEM_2020.csv')
 df1.csv_to_parquet('DADOS\MICRODADOS_ENEM_2020.parquet')
 
-# # Converting ITENS_PROVA_2020 from csv to parquet
 df2 = Convert()
 df2.read_csv('DADOS\ITENS_PROVA_2020.csv')
 df2.csv_to_parquet('DADOS\ITENS_PROVA_2020.parquet')

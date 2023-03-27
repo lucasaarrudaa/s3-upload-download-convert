e7811ad2a1867a479d8bb899f62f0b6e1deae309
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-03-27 13:37:51 -0300

    update main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from conversor import Convert
-from interact_s3 import BucketS3
+from src.conversor import Convert
+from src.interact_s3 import BucketS3
 
 # Download from bucket to local
 b1 = BucketS3('datalake-my-lucas-bucket')
@@ -12,9 +12,12 @@ b2.download('raw-data/ITENS_PROVA_2020.csv',
 
 # Converting from csv to parquet
 df1 = Convert()
-df1.read_csv('DADOS\MICRODADOS_ENEM_2020.csv')
-df1.csv_to_parquet('DADOS\MICRODADOS_ENEM_2020.parquet')
+df1.spark_read_csv('data\MICRODADOS_ENEM_2020.csv')
+df1.spark_csv_to_parquet('s3-upload-download-convert\staging\enem\microdados')
 
 df2 = Convert()
-df2.read_csv('DADOS\ITENS_PROVA_2020.csv')
-df2.csv_to_parquet('DADOS\ITENS_PROVA_2020.parquet')
+df2.spark_read_csv('data\ITENS_PROVA_2020.csv')
+df2.spark_csv_to_parquet('s3-upload-download-convert\staging\enem\itens_prova')
+
+
+

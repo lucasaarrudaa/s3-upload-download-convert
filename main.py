aea6ad644019ef43e382fc68ae0fd7cb1e259994
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-04-06 11:30:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from src.conversor import Convert
 from src.interact_s3 import BucketS3
 
+bucket_name = "datalake-my-lucas-bucket"
+s3_bucket = BucketS3(bucket_name)
+
 # Download from bucket to local
-b1 = BucketS3('datalake-my-lucas-bucket')
-b1.download('raw-data/MICRODADOS_ENEM_2020.csv',
-            'DADOS/MICRODADOS_ENEM_2020.csv')
+a1 = s3_bucket.download("'raw-data/MICRODADOS_ENEM_2020.csv'", "DADOS/MICRODADOS_ENEM_2020.csv")
+a2 = s3_bucket.download("raw-data/ITENS_PROVA_2020.csv", "DADOS/ITENS_PROVA_2020.csv")
 
-b2 = BucketS3('datalake-my-lucas-bucket')
-b2.download('raw-data/ITENS_PROVA_2020.csv',
-            'DADOS/ITENS_PROVA_2020.csv')
+convert = Convert(bucket_name)
 
-# Converting from csv to parquet
-df1 = Convert()
-df1.spark_read_csv('data\MICRODADOS_ENEM_2020.csv')
-df1.spark_csv_to_parquet('s3-upload-download-convert\staging\enem\microdados')
-
-df2 = Convert()
-df2.spark_read_csv('data\ITENS_PROVA_2020.csv')
-df2.spark_csv_to_parquet('s3-upload-download-convert\staging\enem\itens_prova')
-
-
-
+# Reading a CSV file and obtaining a Spark dataframe
+df1 = convert.spark_read_csv("DADOS/MICRODADOS_ENEM_2020.csv")
+df2 = convert.spark_read_csv("cDADOS/ITENS_PROVA_2020.csv")
